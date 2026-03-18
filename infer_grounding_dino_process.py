8e1d44103cdae6114601a95c166ce8a4985650a3
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2026-03-18 10:44:44 +0100

    chore: Qt 6

--- infer_grounding_dino_process.py
+++ infer_grounding_dino_process.py
@@ -1,20 +1,3 @@
-# Copyright (C) 2021 Ikomia SAS
-# Contact: https://www.ikomia.com
-#
-# This file is part of the IkomiaStudio software.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import copy
 import urllib.request
 import os
@@ -224,8 +207,8 @@ class InferGroundingDinoFactory(dataprocess.CTaskFactory):
         self.info.short_description = "Inference of the Grounding DINO model"
         # relative path -> as displayed in Ikomia application process tree
         self.info.path = "Plugins/Python/Detection"
-        self.info.version = "1.1.1"
-        self.info.min_ikomia_version = "0.15.0"
+        self.info.version = "1.2.0"
+        self.info.min_ikomia_version = "0.16.0"
         self.info.icon_path = "icons/icon.png"
         self.info.authors = "Liu, Shilong and Zeng, Zhaoyang and Ren, Tianhe and Li, " \
                             "Feng and Zhang, Hao and Yang, Jie and Li, Chunyuan and Yang, "\
